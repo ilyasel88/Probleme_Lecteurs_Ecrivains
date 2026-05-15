5f0b4cedd917551269980b71e61ad577c0531e78
EL MAZYANI MOHAMED ILYAS <elmazyaniilyas@gmail.com>
2026-05-15 10:28:34 +0100

    Update RessourcePartagee.java

--- src/RessourcePartagee.java
+++ src/RessourcePartagee.java
@@ -5,6 +5,7 @@ public class RessourcePartagee {
     private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock(true);
     private String donnee = "Initial";
     private InterfaceGraphique gui;
+    private static int compteurFile = 0;
     
     public RessourcePartagee(InterfaceGraphique gui) {
         this.gui = gui;
@@ -12,21 +13,43 @@ public class RessourcePartagee {
     }
     
     public void lire(int id) {
+        int position;
+        synchronized(RessourcePartagee.class) {
+            compteurFile++;
+            position = compteurFile;
+        }
+        gui.ajouterMessage("[FILE] Lecteur " + id + " entre en file (position " + position + ")");
+        
         rwLock.readLock().lock();
-        gui.ajouterMessage("Lecteur " + id + " lit: " + donnee);
-        rwLock.readLock().unlock();
+        
+        gui.ajouterMessage("[SORTIE] Lecteur " + id + " sort de la file et lit");
+        gui.ajouterMessage("[LECTURE] Lecteur " + id + " lit: " + donnee);
         
         try { Thread.sleep(100); } catch (Exception e) {}
+        
+        rwLock.readLock().unlock();
+        gui.ajouterMessage("[FIN] Lecteur " + id + " a termine\n");
     }
     
     public void ecrire(int id, String valeur) {
+        int position;
+        synchronized(RessourcePartagee.class) {
+            compteurFile++;
+            position = compteurFile;
+        }
+        gui.ajouterMessage("[FILE] Ecrivain " + id + " entre en file (position " + position + ")");
+        
         rwLock.writeLock().lock();
-        gui.ajouterMessage("Ecrivain " + id + " ecrit: " + valeur);
+        
+        gui.ajouterMessage("[SORTIE] Ecrivain " + id + " sort de la file et ecrit");
+        gui.ajouterMessage("[ECRITURE] Ecrivain " + id + " ecrit: " + valeur);
         donnee = valeur;
         gui.majValeur(valeur);
-        rwLock.writeLock().unlock();
         
         try { Thread.sleep(300); } catch (Exception e) {}
+        
+        rwLock.writeLock().unlock();
+        gui.ajouterMessage("[FIN] Ecrivain " + id + " a termine\n");
     }
     
     public String getDonnee() {
