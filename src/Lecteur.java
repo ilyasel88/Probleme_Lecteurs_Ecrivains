c7d9269eb288846c5063b8561d4cdb9f68e1c8a8
EL MAZYANI MOHAMED ILYAS <elmazyaniilyas@gmail.com>
2026-06-15 23:20:41 +0100

    lecteur

--- src/Lecteur.java
+++ src/Lecteur.java
@@ -13,9 +13,10 @@ public class Lecteur implements Runnable {
         this.simulateur = simulateur;
         this.gui = gui;
     }
+    
     public static synchronized void reinitialiserCompteur() {
-    compteur = 0;
-}
+        compteur = 0;
+    }
     
     public static synchronized int getCompteurLectures() {
         return compteur;
@@ -28,16 +29,22 @@ public class Lecteur implements Runnable {
     public void run() {
         Random rand = new Random();
         
+        // Delai d'arrivee aleatoire (0 a 2000ms)
+        try {
+            Thread.sleep(rand.nextInt(2000));
+        } catch (Exception e) {}
+        
         while (!simulateur.estFini() && !Thread.currentThread().isInterrupted()) {
-            try {
-                Thread.sleep(1500 + rand.nextInt(2000));
-            } catch (Exception e) {
-                break;
-            }
-            
             ressource.lire(id);
             incrementerCompteur();
             gui.majLectures(compteur);
+            
+            // Pause aleatoire avant la prochaine demande (200 a 1500ms)
+            try {
+                Thread.sleep(200 + rand.nextInt(1300));
+            } catch (Exception e) {
+                break;
+            }
         }
         
         gui.ajouterMessage("Lecteur " + id + " arrete");
