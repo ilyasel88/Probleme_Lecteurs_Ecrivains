34b24101fa30f310ba49a89682ba59920fcd8378
EL MAZYANI MOHAMED ILYAS <elmazyaniilyas@gmail.com>
2026-06-15 23:32:25 +0100

    ecrivain

--- src/Ecrivain.java
+++ src/Ecrivain.java
@@ -1,14 +1,15 @@
 import java.util.Random;
 
-public class Lecteur implements Runnable {
+public class Ecrivain implements Runnable {
     private int id;
     private RessourcePartagee ressource;
     private Simulateur simulateur;
     private InterfaceGraphique gui;
-    private static int compteur = 0;
+    private static int compteurEcritures = 0;
+    private static int compteurModifs = 0;
     private Random rand = new Random();
     
-    public Lecteur(int id, RessourcePartagee ressource, Simulateur simulateur, InterfaceGraphique gui) {
+    public Ecrivain(int id, RessourcePartagee ressource, Simulateur simulateur, InterfaceGraphique gui) {
         this.id = id;
         this.ressource = ressource;
         this.simulateur = simulateur;
@@ -16,15 +17,21 @@ public class Lecteur implements Runnable {
     }
     
     public static synchronized void reinitialiserCompteur() {
-        compteur = 0;
+        compteurEcritures = 0;
+        compteurModifs = 0;
     }
     
-    public static synchronized int getCompteurLectures() {
-        return compteur;
+    public static synchronized int getCompteurEcritures() {
+        return compteurEcritures;
     }
     
-    private static synchronized void incrementerCompteur() {
-        compteur++;
+    private static synchronized String getNouvelleValeur(int id) {
+        compteurModifs++;
+        return "Donnee-" + compteurModifs + "-par-" + id;
+    }
+    
+    private static synchronized void incrementerCompteurEcritures() {
+        compteurEcritures++;
     }
     
     public void run() {
@@ -33,18 +40,21 @@ public class Lecteur implements Runnable {
         
         while (!simulateur.estFini() && !Thread.currentThread().isInterrupted()) {
             // 1. Demander l'acces (entrer dans la file)
-            int position = ressource.demanderLecture(id);
+            int position = ressource.demanderEcriture(id);
             
-            // 2. Lire (attend son tour si necessaire)
-            ressource.lire(id, position);
+            // 2. Creer la nouvelle valeur
+            String valeur = getNouvelleValeur(id);
             
-            incrementerCompteur();
-            gui.majLectures(compteur);
+            // 3. Ecrire (attend son tour si necessaire)
+            ressource.ecrire(id, position, valeur);
             
-            // 3. Pause avant la prochaine demande
-            try { Thread.sleep(500 + rand.nextInt(1000)); } catch (Exception e) { break; }
+            incrementerCompteurEcritures();
+            gui.majEcritures(compteurEcritures);
+            
+            // 4. Pause avant la prochaine demande
+            try { Thread.sleep(1000 + rand.nextInt(1500)); } catch (Exception e) { break; }
         }
         
-        gui.ajouterMessage("Lecteur " + id + " arrete");
+        gui.ajouterMessage("Ecrivain " + id + " arrete");
     }
 }
