9e31807ae0cadac29fc874cba720707563de2409
EL MAZYANI MOHAMED ILYAS <elmazyaniilyas@gmail.com>
2026-06-15 23:31:50 +0100

    update

--- src/Ecrivain.java
+++ src/Ecrivain.java
@@ -1,14 +1,14 @@
 import java.util.Random;
 
-public class Ecrivain implements Runnable {
+public class Lecteur implements Runnable {
     private int id;
     private RessourcePartagee ressource;
     private Simulateur simulateur;
     private InterfaceGraphique gui;
-    private static int compteurEcritures = 0;
-    private static int compteurModifs = 0;
+    private static int compteur = 0;
+    private Random rand = new Random();
     
-    public Ecrivain(int id, RessourcePartagee ressource, Simulateur simulateur, InterfaceGraphique gui) {
+    public Lecteur(int id, RessourcePartagee ressource, Simulateur simulateur, InterfaceGraphique gui) {
         this.id = id;
         this.ressource = ressource;
         this.simulateur = simulateur;
@@ -16,45 +16,35 @@ public class Ecrivain implements Runnable {
     }
     
     public static synchronized void reinitialiserCompteur() {
-        compteurEcritures = 0;
-        compteurModifs = 0;
+        compteur = 0;
     }
     
-    public static synchronized int getCompteurEcritures() {
-        return compteurEcritures;
+    public static synchronized int getCompteurLectures() {
+        return compteur;
     }
     
-    private static synchronized String getNouvelleValeur(int id) {
-        compteurModifs++;
-        return "Donnee-" + compteurModifs + "-par-" + id;
-    }
-    
-    private static synchronized void incrementerCompteurEcritures() {
-        compteurEcritures++;
+    private static synchronized void incrementerCompteur() {
+        compteur++;
     }
     
     public void run() {
-        Random rand = new Random();
-        
-        // Delai d'arrivee aleatoire (0 a 2000ms)
-        try {
-            Thread.sleep(rand.nextInt(2000));
-        } catch (Exception e) {}
+        // Pause aleatoire avant la premiere demande
+        try { Thread.sleep(rand.nextInt(500)); } catch (Exception e) {}
         
         while (!simulateur.estFini() && !Thread.currentThread().isInterrupted()) {
-            String valeur = getNouvelleValeur(id);
-            ressource.ecrire(id, valeur);
-            incrementerCompteurEcritures();
-            gui.majEcritures(compteurEcritures);
+            // 1. Demander l'acces (entrer dans la file)
+            int position = ressource.demanderLecture(id);
             
-            // Pause aleatoire avant la prochaine demande (500 a 2000ms)
-            try {
-                Thread.sleep(500 + rand.nextInt(1500));
-            } catch (Exception e) {
-                break;
-            }
+            // 2. Lire (attend son tour si necessaire)
+            ressource.lire(id, position);
+            
+            incrementerCompteur();
+            gui.majLectures(compteur);
+            
+            // 3. Pause avant la prochaine demande
+            try { Thread.sleep(500 + rand.nextInt(1000)); } catch (Exception e) { break; }
         }
         
-        gui.ajouterMessage("Ecrivain " + id + " arrete");
+        gui.ajouterMessage("Lecteur " + id + " arrete");
     }
 }
